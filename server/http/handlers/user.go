cff7a9302cd6d27e9d37f1744686f94f1bf70a5c
fahdjamy <fahdjamy2@gmail.com>
2024-11-20 18:51:13 -0600

    Add a user handler that will create and manager user handlers/routes

---
+++ server/http/handlers/user.go
@@ -0,0 +1,40 @@
+package handlers
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	xrf "xrf197ilz35aq0"
+	"xrf197ilz35aq0/core/exchange"
+	"xrf197ilz35aq0/core/service/user"
+)
+
+type User struct {
+	router      *mux.Router
+	logger      xrf.Logger
+	userManager user.Manager
+}
+
+func NewUser(logger xrf.Logger, userManager user.Manager, router *mux.Router) *User {
+	return &User{
+		router:      router,
+		logger:      logger,
+		userManager: userManager,
+	}
+}
+
+func (user *User) createUser(w http.ResponseWriter, req *http.Request) {
+	var userReq exchange.UserRequest
+
+	jsonErr := decodeJSONBody(req, &userReq)
+	if jsonErr != nil {
+		writeErrorResponse(*jsonErr, w, user.logger)
+		return
+	}
+
+	response := Response{Data: userReq, Code: http.StatusCreated}
+	writeResponse(response, w, user.logger)
+}
+
+func (user *User) RegisterAndListen() {
+	user.router.HandleFunc("/user", user.createUser).Methods("POST")
+}
