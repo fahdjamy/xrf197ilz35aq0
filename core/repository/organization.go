7ac4718cb7c99abb70e4af33b87e4b8b64617ac3
fahdjamy <fahdjamy2@gmail.com>
2025-01-05 16:50:27 -0600

    Add methods GetOrgById and FindByMongoId and their implementation in the OrganizationRepository

--- core/repository/organization.go
+++ core/repository/organization.go
@@ -2,10 +2,13 @@ package repository
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"xrf197ilz35aq0/core/model/org"
 	"xrf197ilz35aq0/internal"
+	"xrf197ilz35aq0/internal/constants"
 	xrfErr "xrf197ilz35aq0/internal/error"
 )
 
@@ -13,6 +16,8 @@ const OrgCollection = "organization"
 
 type OrganizationRepository interface {
 	Create(organization *org.Organization, ctx context.Context) (any, error)
+	GetOrgById(id string, ctx context.Context) (*org.Organization, error)
+	FindByMongoId(id string, ctx context.Context) (*org.Organization, error)
 }
 
 type orgRepo struct {
@@ -20,18 +25,69 @@ type orgRepo struct {
 	log internal.Logger
 }
 
-func (or *orgRepo) Create(organization *org.Organization, ctx context.Context) (any, error) {
+func (repo *orgRepo) Create(organization *org.Organization, ctx context.Context) (any, error) {
 	internalError = &xrfErr.Internal{}
-	document, err := or.db.Collection(OrgCollection).InsertOne(ctx, organization)
+	document, err := repo.db.Collection(OrgCollection).InsertOne(ctx, organization)
 	if err != nil {
-		or.log.Error(fmt.Sprintf("event=mongoDBFailure :: action=saveOrg :: err=%s", err))
+		repo.log.Error(fmt.Sprintf("event=mongoDBFailure :: action=saveOrg :: err=%s", err))
 		internalError.Message = "Creating new org in mongodb failed"
 		internalError.Err = err
 		return nil, err
 	}
-	or.log.Debug(fmt.Sprintf("event=saveOrg :: success=true :: objectID=%v", document.InsertedID))
+	repo.log.Debug(fmt.Sprintf("event=saveOrg :: success=true :: objectID=%v", document.InsertedID))
 
-	return document, nil
+	return document.InsertedID, nil
+}
+
+func (repo *orgRepo) GetOrgById(id string, ctx context.Context) (*org.Organization, error) {
+	internalError = &xrfErr.Internal{}
+	externalError = &xrfErr.External{}
+	internalError.Source = "core/repository/organization#getOrgById"
+
+	filter := bson.M{constants.OrgId: id}
+
+	var result org.Organization
+	resp := repo.db.Collection(OrgCollection).FindOne(ctx, filter)
+
+	if resp.Err() != nil {
+		if errors.Is(resp.Err(), mongo.ErrNoDocuments) {
+			externalError.Message = "Org not found"
+			return nil, externalError
+		}
+		return nil, resp.Err()
+	}
+
+	if err := resp.Decode(&result); err != nil {
+		internalError.Err = err
+		internalError.Message = "Failed to decode org object"
+		repo.log.Error(fmt.Sprintf("event=mongoDBFailure :: action=getOrgById :: err=%s", err))
+		return nil, internalError
+	}
+	return &result, nil
+}
+
+func (repo *orgRepo) FindByMongoId(id string, ctx context.Context) (*org.Organization, error) {
+	internalError = &xrfErr.Internal{}
+
+	filter := bson.M{"_id": id}
+	var result org.Organization
+	resp := repo.db.Collection(OrgCollection).FindOne(ctx, filter)
+
+	if resp.Err() != nil {
+		if errors.Is(resp.Err(), mongo.ErrNoDocuments) {
+			externalError.Message = "Org not found"
+			return nil, externalError
+		}
+		return nil, resp.Err()
+	}
+
+	if err := resp.Decode(&result); err != nil {
+		internalError.Err = err
+		internalError.Message = "Failed to decode org object"
+		repo.log.Error(fmt.Sprintf("event=mongoDBFailure :: action=getOrgById :: err=%s", err))
+		return nil, internalError
+	}
+	return &result, nil
 }
 
 func NewOrganizationRepository(db *mongo.Database, log internal.Logger) OrganizationRepository {
