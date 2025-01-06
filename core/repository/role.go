113c93c971e6b0c9509bcd32bdac24eadfb61b11
fahdjamy <fahdjamy2@gmail.com>
2025-01-05 22:02:11 -0600

    Add logic that creates an index on the role model to make sure the role name is always unique

--- core/repository/role.go
+++ core/repository/role.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
 	"xrf197ilz35aq0/core/model/org"
 	"xrf197ilz35aq0/internal"
 	xrfErr "xrf197ilz35aq0/internal/error"
@@ -28,8 +30,15 @@ type roleRepo struct {
 
 func (repo *roleRepo) SaveRole(role *org.Role, ctx context.Context) (string, error) {
 	internalError = &xrfErr.Internal{}
+	externalError = &xrfErr.External{}
 	document, err := repo.db.Collection(RoleCollection).InsertOne(ctx, role)
 	if err != nil {
+		// Check for the duplicate key error
+		if mongo.IsDuplicateKeyError(err) {
+			repo.log.Error(fmt.Sprintf("event=mongoDBFailure :: action=createRole :: err=duplicateName :: name=%s", role.Name))
+			externalError.Message = "role name already exists"
+			return "", externalError
+		}
 		repo.log.Error(fmt.Sprintf("event=mongoDBFailure :: action=createRole :: err=%s", err))
 		internalError.Message = "Creating new role in mongodb failed"
 		internalError.Err = err
@@ -51,8 +60,27 @@ func (repo *roleRepo) FindRoleById(id string, ctx context.Context) (*org.Role, e
 }
 
 func (repo *roleRepo) FindRoleByName(name string, ctx context.Context) (*org.Role, error) {
-	//TODO implement me
-	panic("implement me")
+	var result org.Role
+	internalError = &xrfErr.Internal{}
+
+	filter := bson.M{"name": name}
+	resp := repo.db.Collection(RoleCollection).FindOne(ctx, filter)
+
+	if resp.Err() != nil {
+		if errors.Is(resp.Err(), mongo.ErrNoDocuments) {
+			externalError.Message = "Role not found"
+			return nil, externalError
+		}
+		return nil, resp.Err()
+	}
+
+	if err := resp.Decode(&result); err != nil {
+		internalError.Err = err
+		internalError.Message = "Failed to decode role object"
+		repo.log.Error(fmt.Sprintf("event=mongoDBFailure :: action=FindRoleByName :: err=%s", err))
+		return nil, internalError
+	}
+	return &result, nil
 }
 
 func (repo *roleRepo) FindRoleByMongoId(mongoId string, ctx context.Context) (*org.Role, error) {
@@ -79,9 +107,28 @@ func (repo *roleRepo) FindRoleByMongoId(mongoId string, ctx context.Context) (*o
 	return &result, nil
 }
 
-func NewRoleRepo(db *mongo.Database, log internal.Logger) RoleRepo {
+func NewRoleRepo(db *mongo.Database, log internal.Logger) (RoleRepo, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	indexModel := mongo.IndexModel{
+		// create an index on the name field in ascending order (1)
+		Keys: bson.D{{Key: "name", Value: 1}},
+		// sets the unique option to true, enforcing uniqueness for role name.
+		Options: options.Index().SetUnique(true),
+	}
+
+	// creates the index on the 'Role' collection
+	_, err := db.Collection(RoleCollection).Indexes().CreateOne(ctx, indexModel)
+	if err != nil {
+		return nil, &xrfErr.Internal{
+			Err:     err,
+			Message: "Failed to create index",
+			Source:  "core/repository/role#NewRoleRepo",
+		}
+	}
 	return &roleRepo{
 		db:  db,
 		log: log,
-	}
+	}, nil
 }
