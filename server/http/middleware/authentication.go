60253315a0b94b01f05413eb54f7bec806223b82
fahdjamy <fahdjamy2@gmail.com>
2025-04-17 18:51:09 -0500

    Create a default org in a separate go routine function

--- server/http/middleware/authentication.go
+++ server/http/middleware/authentication.go
@@ -19,23 +19,38 @@ type AuthenticationMiddleware struct {
 
 func (m *AuthenticationMiddleware) Handle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		authToken := r.Header.Get(XrfAuthToken)
-		if authToken == "" {
-			externalErr := &xrfErr.External{Message: "missing xrf-auth-token authToken", Code: 401}
-			response.WriteErrorResponse(externalErr, w, m.logger)
-			return
-		}
+		if m.shouldCheckRouteAuth(r) {
+			authToken := r.Header.Get(XrfAuthToken)
+			if authToken == "" {
+				externalErr := &xrfErr.External{Message: "missing xrf-auth-token authToken", Code: 401}
+				response.WriteErrorResponse(externalErr, w, m.logger)
+				return
+			}
 
-		ctx := context.WithValue(r.Context(), XrfAuthToken, authToken)
-		userId, err := m.authService.VerifyToken(authToken, ctx)
-		if err != nil {
-			response.WriteErrorResponse(err, w, m.logger)
-			return
+			ctx := context.WithValue(r.Context(), XrfAuthToken, authToken)
+			userId, err := m.authService.VerifyToken(authToken, ctx)
+			if err != nil {
+				response.WriteErrorResponse(err, w, m.logger)
+				return
+			}
+			m.logger.Debug(fmt.Sprintf("event=authenticatingToken :: userId=%s", userId))
 		}
-		m.logger.Debug(fmt.Sprintf("event=authenticatingToken :: userId=%s", userId))
 
 		next.ServeHTTP(w, r)
 	})
+}
+
+func (m *AuthenticationMiddleware) shouldCheckRouteAuth(r *http.Request) bool {
+	unCheckedRoutes := make(map[string]struct{})
+	unCheckedRoutes["/health"] = struct{}{}
+
+	route := r.URL.Path
+
+	if _, ok := unCheckedRoutes[route]; !ok {
+		return true
+	}
+
+	return false
 }
 
 func NewAuthenticationMiddleware(logger internal.Logger, authService service.AuthService) AuthenticationMiddleware {
